f76645a1461fc13960331912acef509f7fce3f7c
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-09 10:02:27 -0600

    API Server Setup

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ root.render(<App />);
 // ➜  media git:(master) npm install @faker-js/faker @reduxjs/toolkit axios classnames json-server react-icons
 // ➜  media git:(master) ✗ npm install -D tailwindcss postcss autoprefixer
 // ➜  media git:(master) ✗ npx tailwindcss init -p
+// ➜  media git:(master) npm run start:server
