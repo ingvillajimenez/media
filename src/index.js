d252af077f61fd939ecd85fed2e9476178a768f3
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-09 09:40:24 -0600

    Adding a Few Dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +15,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// ➜  media git:(master) npm install @faker-js/faker @reduxjs/toolkit axios classnames json-server react-icons
+// ➜  media git:(master) ✗ npm install -D tailwindcss postcss autoprefixer
+// ➜  media git:(master) ✗ npx tailwindcss init -p
