cd1d4fae93e69b5acf4ce97d2e7b07b28b0757a8
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-09 09:49:15 -0600

    Initial App Boilerplate

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById("root");
+const root = createRoot(el);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
 
 // ➜  media git:(master) npm install @faker-js/faker @reduxjs/toolkit axios classnames json-server react-icons
 // ➜  media git:(master) ✗ npm install -D tailwindcss postcss autoprefixer
