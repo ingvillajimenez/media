8c2d3e93cc0cd5bd70366b3d934f13b1479e63f1
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-09 10:42:12 -0600

    Creating the Redux Store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import App from "./App";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // ➜  media git:(master) npm install @faker-js/faker @reduxjs/toolkit axios classnames json-server react-icons
 // ➜  media git:(master) ✗ npm install -D tailwindcss postcss autoprefixer
 // ➜  media git:(master) ✗ npx tailwindcss init -p
 // ➜  media git:(master) npm run start:server
+// ➜  media git:(master) npm install react-redux
