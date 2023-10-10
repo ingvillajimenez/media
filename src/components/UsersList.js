4dc86f54923e18b7f06a35a26f3602266440d20b
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-10 10:49:43 -0600

    Local Fine-Grained Loading State

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,28 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
   const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users; // { data: [], isLoading: false, error: null }
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .then(() => {
+        console.log("SUCCESS");
+      })
+      .catch(() => {
+        console.log("FAIL");
+      });
+
+    // BAD!!
+    // setIsLoadingUsers(false);
   }, [dispatch]);
 
   const handleUserAdd = () => {
     dispatch(addUser());
   };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
